9fd39f9863f5ce2d304e9df093d9abd062b7961a
BrodyPlaysYT <63876947+BrodyPlaysYT@users.noreply.github.com>
2020-05-18 16:32:59 +1000

    Created the RamBrowse Web Browser

--- RamBrowse.py
+++ RamBrowse.py
@@ -2,99 +2,254 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtWebEngineWidgets import *
-import sys
+from PyQt5.QtPrintSupport import *
+
 import os
+import sys
+
+
+class AboutDialog(QDialog):
+    def __init__(self, *args, **kwargs):
+        super(AboutDialog, self).__init__(*args, **kwargs)
+
+        QBtn = QDialogButtonBox.Ok  # No cancel
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        layout = QVBoxLayout()
+
+        title = QLabel("RamBrowse")
+        font = title.font()
+        font.setPointSize(20)
+        title.setFont(font)
+
+        layout.addWidget(title)
+
+        logo = QLabel()
+        logo.setPixmap(QPixmap(os.path.join('images', 'Logo.png')))
+        layout.addWidget(logo)
+
+        layout.addWidget(QLabel("Version 1.0.0"))
+        layout.addWidget(QLabel("Copyright 2020 DoesArt Studios"))
+
+        for i in range(0, layout.count()):
+            layout.itemAt(i).setAlignment(Qt.AlignHCenter)
+
+        layout.addWidget(self.buttonBox)
+
+        self.setLayout(layout)
+
 
 class MainWindow(QMainWindow):
-    def __init__(self):
-        QMainWindow.__init__(self)
-        self.mainfolder = os.path.dirname(__file__)
-        self.img_folder = os.path.join(self.mainfolder, "icons")
-        self.setWindowTitle("RamBrowse")
-        self.setGeometry(150,50,900,600)
-        self.setWindowIcon(QIcon(os.path.join(self.img_folder,"Logo.png")))
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
 
-        self.browser =  QWebEngineView()
-        self.browser.load(QUrl("http://duckduckgo.com/"))
-        self.browser.show()
+        self.tabs = QTabWidget()
+        self.tabs.setDocumentMode(True)
+        self.tabs.tabBarDoubleClicked.connect(self.tab_open_doubleclick)
+        self.tabs.currentChanged.connect(self.current_tab_changed)
+        self.tabs.setTabsClosable(True)
+        self.tabs.tabCloseRequested.connect(self.close_current_tab)
 
-        self.setCentralWidget(self.browser)
+        self.setCentralWidget(self.tabs)
 
-        # creation of tool bar ....
+        self.status = QStatusBar()
+        self.setStatusBar(self.status)
+
         navtb = QToolBar("Navigation")
         navtb.setIconSize(QSize(16, 16))
         self.addToolBar(navtb)
 
-        back_btn = QAction(QIcon(os.path.join(self.img_folder, "arrow-curve-180-left.png")), "Back", self)
-        back_btn.setStatusTip("previous tab")
-        back_btn.triggered.connect(self.browser.back)
+        back_btn = QAction(QIcon(os.path.join('images', 'arrow-180.png')), "Back", self)
+        back_btn.setStatusTip("Back to previous page")
+        back_btn.triggered.connect(lambda: self.tabs.currentWidget().back())
         navtb.addAction(back_btn)
 
-        forward_btn = QAction(QIcon(os.path.join(self.img_folder, "arrow-curve.png")), "forward", self)
-        forward_btn.setStatusTip("Forward tab")
-        forward_btn.triggered.connect(self.browser.forward)
-        navtb.addAction(forward_btn)
+        next_btn = QAction(QIcon(os.path.join('images', 'arrow-000.png')), "Forward", self)
+        next_btn.setStatusTip("Forward to next page")
+        next_btn.triggered.connect(lambda: self.tabs.currentWidget().forward())
+        navtb.addAction(next_btn)
 
-        home_btn = QAction(QIcon(os.path.join(self.img_folder, "home.png")), "Home", self)
-        home_btn.setStatusTip("Home")
-        home_btn.triggered.connect(self.home_clicked)
+        reload_btn = QAction(QIcon(os.path.join('images', 'arrow-circle-315.png')), "Reload", self)
+        reload_btn.setStatusTip("Reload page")
+        reload_btn.triggered.connect(lambda: self.tabs.currentWidget().reload())
+        navtb.addAction(reload_btn)
+
+        home_btn = QAction(QIcon(os.path.join('images', 'home.png')), "Home", self)
+        home_btn.setStatusTip("Go home")
+        home_btn.triggered.connect(self.navigate_home)
         navtb.addAction(home_btn)
 
         navtb.addSeparator()
 
-        self.http_icon = QLabel()
-        self.http_icon.setPixmap(QPixmap(os.path.join(self.img_folder, "lock.png")))
-        navtb.addWidget(self.http_icon)
+        self.httpsicon = QLabel()  # Yes, really!
+        self.httpsicon.setPixmap(QPixmap(os.path.join('images', 'lock-nossl.png')))
+        navtb.addWidget(self.httpsicon)
 
         self.urlbar = QLineEdit()
-        self.urlbar.returnPressed.connect(self.navigate_url)
+        self.urlbar.returnPressed.connect(self.navigate_to_url)
         navtb.addWidget(self.urlbar)
 
-        search_btn = QAction(QIcon(os.path.join(self.img_folder, "search.png")), "Search", self)
-        search_btn.setStatusTip("Search")
-        search_btn.triggered.connect(self.navigate_url)
-        navtb.addAction(search_btn)
-
-        refresh_btn = QAction(QIcon(os.path.join(self.img_folder, "arrow-circle-double-135.png")), "Refresh", self)
-        refresh_btn.setStatusTip("Refresh")
-        refresh_btn.triggered.connect(self.browser.reload)
-        navtb.addAction(refresh_btn)
-
-        stop_btn = QAction(QIcon(os.path.join(self.img_folder, "cross.png")), "Stop", self)
-        stop_btn.setStatusTip("Stop")
-        stop_btn.triggered.connect(self.browser.stop)
+        stop_btn = QAction(QIcon(os.path.join('images', 'cross-circle.png')), "Stop", self)
+        stop_btn.setStatusTip("Stop loading current page")
+        stop_btn.triggered.connect(lambda: self.tabs.currentWidget().stop())
         navtb.addAction(stop_btn)
 
-        # changing URL bar content per search ...
-        self.browser.urlChanged.connect(self.update_urlbar)
+        # Uncomment to disable native menubar on Mac
+        # self.menuBar().setNativeMenuBar(False)
 
-    def update_urlbar(self,u):
-        if u.scheme() == 'https':
-            self.http_icon.setPixmap(QPixmap(os.path.join(self.img_folder, "lock-ssl.png")))
+        file_menu = self.menuBar().addMenu("&File")
+
+        new_tab_action = QAction(QIcon(os.path.join('images', 'ui-tab--plus.png')), "New Tab", self)
+        new_tab_action.setStatusTip("Open a new tab")
+        new_tab_action.triggered.connect(lambda _: self.add_new_tab())
+        file_menu.addAction(new_tab_action)
+
+        open_file_action = QAction(QIcon(os.path.join('images', 'disk--arrow.png')), "Open file...", self)
+        open_file_action.setStatusTip("Open from file")
+        open_file_action.triggered.connect(self.open_file)
+        file_menu.addAction(open_file_action)
+
+        save_file_action = QAction(QIcon(os.path.join('images', 'disk--pencil.png')), "Save Page As...", self)
+        save_file_action.setStatusTip("Save current page to file")
+        save_file_action.triggered.connect(self.save_file)
+        file_menu.addAction(save_file_action)
+
+        print_action = QAction(QIcon(os.path.join('images', 'printer.png')), "Print...", self)
+        print_action.setStatusTip("Print current page")
+        print_action.triggered.connect(self.print_page)
+        file_menu.addAction(print_action)
+
+        help_menu = self.menuBar().addMenu("&Help")
+
+        about_action = QAction(QIcon(os.path.join('images', 'question.png')), "About RamBrowse", self)
+        about_action.setStatusTip("Find out more about RamBrowse")  # Hungry!
+        about_action.triggered.connect(self.about)
+        help_menu.addAction(about_action)
+
+        navigate_mozarella_action = QAction(QIcon(os.path.join('images', 'lifebuoy.png')),
+                                            "RamBrowse Homepage", self)
+        navigate_mozarella_action.setStatusTip("Go to DoesArt Studios Homepage")
+        navigate_mozarella_action.triggered.connect(self.navigate_mozarella)
+        help_menu.addAction(navigate_mozarella_action)
+
+        self.add_new_tab(QUrl('http://duckduckgo.com'), 'Homepage')
+
+        self.show()
+
+        self.setWindowTitle("RamBrowse")
+        self.setWindowIcon(QIcon(os.path.join('images', 'Logo.png')))
+
+    def add_new_tab(self, qurl=None, label="Blank"):
+
+        if qurl is None:
+            qurl = QUrl('https://duckduckgo.com/')
+
+        browser = QWebEngineView()
+        browser.setUrl(qurl)
+        i = self.tabs.addTab(browser, label)
+
+        self.tabs.setCurrentIndex(i)
+
+        # More difficult! We only want to update the url when it's from the
+        # correct tab
+        browser.urlChanged.connect(lambda qurl, browser=browser:
+                                   self.update_urlbar(qurl, browser))
+
+        browser.loadFinished.connect(lambda _, i=i, browser=browser:
+                                     self.tabs.setTabText(i, browser.page().title()))
+
+    def tab_open_doubleclick(self, i):
+        if i == -1:  # No tab under the click
+            self.add_new_tab()
+
+    def current_tab_changed(self, i):
+        qurl = self.tabs.currentWidget().url()
+        self.update_urlbar(qurl, self.tabs.currentWidget())
+        self.update_title(self.tabs.currentWidget())
+
+    def close_current_tab(self, i):
+        if self.tabs.count() < 2:
+            return
+
+        self.tabs.removeTab(i)
+
+    def update_title(self, browser):
+        if browser != self.tabs.currentWidget():
+            # If this signal is not from the current tab, ignore
+            return
+
+        title = self.tabs.currentWidget().page().title()
+        self.setWindowTitle("%s - RamBrowse" % title)
+
+    def navigate_mozarella(self):
+        self.tabs.currentWidget().setUrl(QUrl("https://DoesArt-Studios.github.io/RamBrowseWebsite/"))
+
+    def about(self):
+        dlg = AboutDialog()
+        dlg.exec_()
+
+    def open_file(self):
+        filename, _ = QFileDialog.getOpenFileName(self, "Open file", "",
+                                                  "Hypertext Markup Language (*.htm *.html);;"
+                                                  "All files (*.*)")
+
+        if filename:
+            with open(filename, 'r') as f:
+                html = f.read()
+
+            self.tabs.currentWidget().setHtml(html)
+            self.urlbar.setText(filename)
+
+    def save_file(self):
+        filename, _ = QFileDialog.getSaveFileName(self, "Save Page As", "",
+                                                  "Hypertext Markup Language (*.htm *html);;"
+                                                  "All files (*.*)")
+
+        if filename:
+            html = self.tabs.currentWidget().page().toHtml()
+            with open(filename, 'w') as f:
+                f.write(html.encode('utf8'))
+
+    def print_page(self):
+        dlg = QPrintPreviewDialog()
+        dlg.paintRequested.connect(self.browser.print_)
+        dlg.exec_()
+
+    def navigate_home(self):
+        self.tabs.currentWidget().setUrl(QUrl("http://duckduckgo.com"))
+
+    def navigate_to_url(self):  # Does not receive the Url
+        q = QUrl(self.urlbar.text())
+        if q.scheme() == "":
+            q.setScheme("http")
+
+        self.tabs.currentWidget().setUrl(q)
+
+    def update_urlbar(self, q, browser=None):
+
+        if browser != self.tabs.currentWidget():
+            # If this signal is not from the current tab, ignore
+            return
+
+        if q.scheme() == 'https':
+            # Secure padlock icon
+            self.httpsicon.setPixmap(QPixmap(os.path.join('images', 'lock-ssl.png')))
+
         else:
-            self.http_icon.setPixmap(QPixmap(os.path.join(self.img_folder, "lock.png")))
-        u= str(u)
+            # Insecure padlock icon
+            self.httpsicon.setPixmap(QPixmap(os.path.join('images', 'lock-nossl.png')))
 
-        self.urlbar.setText(u[19:-2])
+        self.urlbar.setText(q.toString())
         self.urlbar.setCursorPosition(0)
 
-    def navigate_url(self):
-        u = QUrl(self.urlbar.text())
-        try:
-            if u.scheme() == "":
-                u.setScheme('http')
 
-            self.browser.load(u)
-            self.browser.show()
-        except Exception as e:
-            print(e)
+app = QApplication(sys.argv)
+app.setApplicationName("RamBrowse")
+app.setOrganizationName("DoesArt Studios")
+app.setOrganizationDomain("DoesArt-Studios.Github.io/RamBrowsewebsite/")
 
-    def home_clicked(self):
-        self.browser.load(QUrl("http://duckduckgo.com/"))
-        self.browser.show()
+window = MainWindow()
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    mainWin =MainWindow()
-    mainWin.show()
-    sys.exit(app.exec_())
+app.exec_()
